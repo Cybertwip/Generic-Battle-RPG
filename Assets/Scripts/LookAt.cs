b0118191d508fd8175d8918cac00627a35452a96
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2020-06-03 23:32:09 -0700

    Oh yeah...optimization
    ...I forgot (then wondered...lazily...until I checked) that `Vector3.Angle` returns an **unsigned** float. That means `Mathf.Abs(lookAngle)` is a wasted bunch of computations (damn the standard libraries and their superflous edge-case checks!).
    
    So yeah. I took that off again.

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -16,9 +16,8 @@ public class LookAt : MonoBehaviour
 		previous_rotation = transform.rotation;
         Vector3 lookDirection =  target.position + 1.75f * Vector3.up - transform.position;
         float lookAngle = Vector3.Angle(-Vector3.forward, lookDirection);
-		//Debug.Log(lookAngle + "    " + lookDirection);
         Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-        if (Mathf.Abs(lookAngle) < 80f)
+        if (lookAngle < 80f)
 		{
             transform.rotation = rotation;
 		}
