b66ffbf2746fb905d96bdcab5cc05ff2f76d51c5
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2020-06-03 23:21:11 -0700

    Fixed thwomp's state machine, and his eyes, and his entire prefab, code, etc.
    What a wonderful deluge of $#*%!
    
    I discovered that his "Look-At" script was not working because his eyes were stuck in place;
    I discovered that his eyes were stuck in place because I forgot it is neccessary to "Unpack" parts of an .fbx prefab I want to edit;
    I only discovered this after re-importing Thwomp with new settings, but this also fixed his orientation issue;
    With his orientation fixed, I simplified the Look-At script.
    
    Also, his old Material was better so I fixed that too.
    Corrected his Animation state-machine.
    
    Everything is wonderful now!

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class LookAt : MonoBehaviour
 {
     private Transform target;
+	private Quaternion previous_rotation;
 
     private void Awake()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
-
-    void Update()
-    {
-        //NOTE: The character's coordinate system is rotated 90 degrees clockwise (fuck)
-        //so need to do adjustments here or reimport entire model
-        
+	void Update()
+	{
+		previous_rotation = transform.rotation;
         Vector3 lookDirection =  target.position + 1.75f * Vector3.up - transform.position;
-        float lookAngle = Vector3.Angle(-transform.right, lookDirection); // first input would normally be transform.forward
+        float lookAngle = Vector3.Angle(-Vector3.forward, lookDirection);
+		//Debug.Log(lookAngle + "    " + lookDirection);
         Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-        if (lookAngle < 80f) // doesn't work, ask around
-            transform.rotation = rotation*Quaternion.Euler(0,90,0);
+        if (Mathf.Abs(lookAngle) < 80f)
+		{
+            transform.rotation = rotation;
+		}
+		else transform.rotation = previous_rotation;
     }
 }
