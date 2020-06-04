c2dc5123f16f1055ea8f5d1cef18671f7dde924c
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2020-06-04 04:33:04 -0700

    Accidentally created a neat idle animation for Thwomp
    Created 2 scripts:
    AI_Thwomp.cs
    Behaviour_Thwomp.cs
    
    The latter isn't good for anything yet.
    The former has an outline of his Actions AI.
    
    I seem to have fucked-up Thwomp's smoothing settings while I was making shape keys. I need to go back into blender and harden all the profile edges, then re-import again, rebuild the character, put his components back on. Geeze!
    
    Deleted a bunch of superflous materials. We can still do better.

---
+++ Assets/Scripts/Enemy_AI/AI_Thwomp.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Thwomp : MonoBehaviour
+{
+    public Animator animator;
+    public Enemy enemyStatus;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // testing some animations, was testing the Behaviours, big waste of time
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle_Pout")) animator.SetTrigger("trigger_grimmace");
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle_Grimmace")) animator.SetTrigger("trigger_return_to_pout");
+    }
+}
+
+// Thwomps available actions are 
+
+// LAUGH, QUAKE, THWOMP, BITE, EAT, TANTRUM, HEAVE
+
+/*  if (last player action was an attack that did no damage)
+    {
+        Laugh();
+        pity_the_fool = true;
+    }
+
+    randF = GetRandFloat(); //0-1f
+
+    if (HP > enemyStatus.MaxHP*0.5f)
+    {
+        // 25% LAUGH, 37.5% QUAKE, 37.5% THWOMP
+        if (randF < 0.250f)
+        {
+            if (pity_the_fool == false) Laugh(); // no effect
+            else pity_the_fool = false; // doesn't laugh twice in a row, resets flag
+        }
+    else if (randomInt < 0.625f) Quake(); // inflicts FEAR status
+    else Thwomp(); // -50 HP
+    }
+
+    else // Thwomp's HP <= enemyStatus.MaxHP*0.5f
+    {
+        // 5% EAT, 10% BITE, 20% LAUGH, 30% HEAVE/TANTRUM, 35% THWOMP
+        if (randF < 0.05f) Eat(); // removes party member, refills Thwomp's MP, instant KO.
+        else if (randF < 0.15f) Bite(); // Thwomp gains 10 FP, player loses 10 FP and reduces player's HP to MathF.Max(Mathf.RoundToInt((PartyMember.MaxHP * 0.1f)), 1)
+        else if (randF < 0.35f)
+        {
+            if (pity_the_fool == false) Laugh(); // no effect
+            else pity_the_fool = false; // doesn't laugh twice in a row, resets flag
+        }
+    }
+    else if (randF < 0.65f)
+    {
+        if (player's last turn was a magic attack) Heave() // Thwomp huffs-and-puffs, takes 2 turns for this this turn and the next.
+        else Tantrum(); // inficts FEAR status and random damage to all party members
+    }
+    else Thwomp(); // -50 HP
+}
+*/
---
+++ Assets/Scripts/Enemy_AI/Behaviour_Thwomp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// worthless so far.
+public class Behaviour_Thwomp : StateMachineBehaviour
+{
+    AnimatorStateInfo currentStateInfo;
+    AnimatorStateInfo previousStateInfo;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        currentStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        previousStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+    }
+}
