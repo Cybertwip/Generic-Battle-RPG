3a2801d8de169b72a4d734b9c16ac5acaf4cf50d
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2020-07-30 18:08:56 -0700

    more tinkering

--- Assets/TimeOut/Scripts/DoorController.cs
+++ Assets/TimeOut/Scripts/DoorController.cs
@@ -9,45 +9,44 @@ public class DoorController : MonoBehaviour
     float angle = 0.0f;
     float yVelocity = 0.0f;
     float smoothTime = 0.3f;
-    //string doorSuffix = "";
-    // Start is called before the first frame update
+    
     void Start()
     {
         DoorEvents.current.onDoorwayTriggerEnter += OnDoorwayOpen;
         DoorEvents.current.onDoorwayTriggerExit += OnDoorwayClose;
-        angle = gameObject.transform.localRotation.y;
-        //doorSuffix = gameObject.name.Substring(gameObject.name.Length - 3);
+        angle = gameObject.transform.localRotation.eulerAngles.y;
     }
 
     private void OnDoorwayOpen(int id)
     {
-        if (id == this.id && angle == 0f) StartCoroutine(DoorOpen(angle, -90f, 0.3f));
+        if (id == this.id && angle == 0f) StartCoroutine( DoorOpen(angle, -90f, smoothTime));
     }
 
     private void OnDoorwayClose(int id)
     {
-        if (id == this.id && angle == -90f) StartCoroutine(DoorClose(angle, 0f, 0.3f));
+        if (id == this.id && angle == -90f) StartCoroutine(DoorClose(angle, 0f, smoothTime));
     }
 
     IEnumerator DoorOpen(float angle, float target, float smoothTime)
     {
-        while (angle > target) // local y-angle 0f --> -90f
+        while (!Mathf.Approximately(angle,target)/*angle > target*/) // local y-angle 0f --> -90f
         {
-            angle = Mathf.SmoothDampAngle(angle, target, ref yVelocity, smoothTime);
+            this.angle = Mathf.SmoothDampAngle(angle, target, ref yVelocity, smoothTime);
+            Debug.Log(angle + ", " + this.angle);
             yield return null;
         }
-        angle = target;
+        this.angle = target;
         yield return 0;
     }
 
     IEnumerator DoorClose(float angle, float target, float smoothTime)
     {
-        while (angle < target) // local y-angle -90f --> 0f
+        while (!Mathf.Approximately(angle, target)/*angle < target*/) // local y-angle -90f --> 0f
         {
-            angle = Mathf.SmoothDampAngle(angle, target, ref yVelocity, smoothTime);
-            yield return null;
+            this.angle = Mathf.SmoothDampAngle(angle, target, ref yVelocity, smoothTime);
+            yield return angle;
         }
-        angle = target;
-        yield return 0;
+        this.angle = target;
+        yield return angle;
     }
 }
